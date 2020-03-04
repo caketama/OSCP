bba2be61e5984daa9a9078062aeea4cf7e1c6a3e
kali <jarrettw@protonmail.com>
2020-03-04 12:27:14 -0500

    added labs directory

---
+++ labs/png/pngtohtml.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Bash script to examine the scan results through HTML.
+
+echo "<HTML><BODY><BR>" > web.html
+
+ls *.png | awk -F : '{print $1":\n<BR><IMG SRC=\""$1""$2"\" width=600><BR>"}' >> web.html
+
+echo "</BODY></HTML>" >> web.html
