b7eeb7d6c6be1c771fdd2554978b771d4fe88b70
kali <jarrettw@protonmail.com>
2020-03-05 03:01:11 -0500

    cleaning up

--- labs/png/pngtohtml.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Bash script to examine the scan results through HTML.
-
-echo "<HTML><BODY><BR>" > web.html
-
-ls *.png | awk -F : '{print $1":\n<BR><IMG SRC=\""$1""$2"\" width=600><BR>"}' >> web.html
-
-echo "</BODY></HTML>" >> web.html
